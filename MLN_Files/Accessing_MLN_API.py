ca9947e1d27d243dd0de2a75f69c93c1837f5bc6
srias292 <srias292@.student.otago.ac.nz>
2022-03-08 20:40:26 +1300

    MLN files added

---
+++ MLN_Files/Accessing_MLN_API.py
@@ -0,0 +1,34 @@
+from mlnQueryTool import MLNInfer
+
+inf = MLNInfer()
+# mlnFiles = ["smoking.mln"]
+# db = "smokingDB.db"
+mlnFiles = ["trial26.mln"]
+db = "trial21.db"  
+
+queries = "cooperate(Skolem, SecureCity), individual_ethos(Skolem,Ethos1), individual_ethos(Skolem,Ethos2)"
+# queries = "Cancer"
+# queries = " individual_ethos(x, Ethos2) ^ cooperate(x, SecureCity)"
+# queries = "individual_ethos(x, Ethos1) ^ cooperate(x, SecureCity)"
+output_filename = "results.txt"
+allResults = {}
+tasks = (("exact inference", "PyMLNs"), ("MC-SAT", "J-MLNs"))
+# tasks = (("MC-SAT", "PyMLNs"), ("MC-SAT", "J-MLNs"))
+for method, engine in tasks[:-1]:
+    allResults[(method, engine)] = inf.run(
+        mlnFiles,
+        db,
+        method,
+        queries,
+        engine,
+        output_filename,
+        saveResults=True,
+        maxSteps=100,
+    )
+
+for (method, engine), results in allResults.iteritems():
+    print("Results obtained using %s and %s" % (engine, method))
+    for atom, p in results.iteritems():
+        print("  %.6f  %s" % (p, atom))
+
+
